dd7491ba1efde709a3992e8cfadbb52598209a3e
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-01-07 12:51:50 -0300

    conclusão do desenvolvimento das funções que permitem fixar e desafixar as notas

--- js/scripts.js
+++ js/scripts.js
@@ -3,70 +3,93 @@ const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 
-
 // Funções
 
 function showNotes() {
-    getNotes().forEach((note) => {
-        const noteElement = createNode(note.id, note.content, note.fixed);
-
-        notesContainer.appendChild(noteElement);
-    });
-};
-
-function addNote() {
-    const notes = getNotes();
-    console.log("Adcionando")
-    console.log(noteInput.value)
-
-    const noteObject = {
-        id: generateId(),
-        content: noteInput.value,
-        fixed: false,
-    };
-
-    const noteElement = createNode(noteObject.id, noteObject.content);
+  getNotes().forEach((note) => {
+    const noteElement = createNode(note.id, note.content, note.fixed);
 
     notesContainer.appendChild(noteElement);
-    
-    notes.push(noteObject);
+  });
+}
 
-    saveNotes(notes);
+function addNote() {
+  const notes = getNotes();
+  console.log("Adcionando");
+  console.log(noteInput.value);
 
-    noteInput.value = "";
-};
+  const noteObject = {
+    id: generateId(),
+    content: noteInput.value,
+    fixed: false,
+  };
+
+  const noteElement = createNode(noteObject.id, noteObject.content);
+
+  notesContainer.appendChild(noteElement);
+
+  notes.push(noteObject);
+
+  saveNotes(notes);
+
+  noteInput.value = "";
+}
 
 function generateId() {
-    return Math.floor(Math.random() * 5000);
-};
+  return Math.floor(Math.random() * 5000);
+}
 
 function createNode(id, content, fixed) {
-    const element = document.createElement("div");
-    element.classList.add("note");
-    const textarea = document.createElement("textarea");
-    textarea.value = content;
-    textarea.placeholder = "Adicione alguma coisa...";
-    element.appendChild(textarea);
-    
-        return element;
-};
+  const element = document.createElement("div");
+  element.classList.add("note");
+  const textarea = document.createElement("textarea");
+  textarea.value = content;
+  textarea.placeholder = "Adicione alguma coisa...";
+  element.appendChild(textarea);
+
+  const pinIcon = document.createElement("i");
+
+  pinIcon.classList.add(...["bi", "bi-pin"]);
+
+  element.appendChild(pinIcon);
+
+  if (fixed) {
+    element.classList.add("fixed");
+  }
+
+  // Eventos do elemento
+
+  element.querySelector(".bi-pin").addEventListener("click", () => {
+    toggleFixNote(id);
+  });
+
+  return element;
+}
+
+function toggleFixNote(id) {
+  const notes = getNotes();
+
+  const targetNotes = notes.filter((note) => note.id === id)[0];
+
+  targetNotes.fixed = !targetNotes.fixed;
+
+  saveNotes(notes);
+}
 
 // LocalStorage
 
 function getNotes() {
-    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+  const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 
-        return notes;
+  return notes;
 }
 
 function saveNotes(notes) {
-    localStorage.setItem("notes", JSON.stringify(notes))
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
-
 
 // Eventos
 addNoteBtn.addEventListener("click", () => addNote());
-
 
 // Start da aplicação
 showNotes();
